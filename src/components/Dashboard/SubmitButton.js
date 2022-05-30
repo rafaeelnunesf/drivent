2acbd65489a3368eb599918db16726f6301c3db4
rafaeelnunes <rafaelnfsq@gmail.com>
2022-05-30 10:43:33 -0300

    feat: submitButton creation

---
+++ src/components/Dashboard/SubmitButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const SubmitButton = styled.button`
+  all: unset;
+  width: 162px;
+  height: 37px;
+  left: 341px;
+  top: 552px;
+
+  background: #e0e0e0;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+
+  color: #000000;
+  cursor: pointer;
+`;
+
+export default SubmitButton;
