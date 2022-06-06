6c149de9b8ceb652958c5392a72d42ceacc25fff
diego-carneiro <diegosantos.cs@gmail.com>
2022-06-06 18:48:20 -0300

    feat: card screen

--- src/pages/Dashboard/Payment/Confirmation.js
+++ src/pages/Dashboard/Payment/Confirmation.js
@@ -1,9 +1,43 @@
+import React, { useState } from 'react';
 import PageTitle from '../../../components/Dashboard/PageTitle';
+import Button from '../../../components/Dashboard/Button';
+import SubtitleInfo from '../../../components/Dashboard/SubtitleInfo';
+import SubmitButton from '../../../components/Dashboard/SubmitButton';
+import Card from '../../../components/Card';
 
-export default function Confirmation() {
-  return(
+export default function Confirmation({ total, formData }) {
+  const [card, setCard] = useState({
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+  });
+
+  const handleInputFocus = (e) => {
+    setCard({ focus: e.target.name });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setCard({ [name]: value });
+  };
+
+  return (
     <>
       <PageTitle>Ingresso e pagamento</PageTitle>
+      <SubtitleInfo>Ingresso escolhido</SubtitleInfo>
+      <Button>
+        {formData.ticketType === 'Presencial'
+          ? `${formData.ticketType} + ${formData.accommodationType} R$ ${total}`
+          : `${formData.ticketType} R$ ${total}`}
+      </Button>
+      <SubtitleInfo>Pagamento</SubtitleInfo>
+      <Card />
+      <SubmitButton>
+        FINALIZAR PAGAMENTO
+      </SubmitButton>
     </>
   );
 }
