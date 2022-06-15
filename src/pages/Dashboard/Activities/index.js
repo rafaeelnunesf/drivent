3ac9afd2db0c5078f40b5436ceb19987b83e680d
Felipe <mr.tvz569@gmail.com>
2022-06-15 19:40:30 -0300

    feat: date buttons added with integration with backend

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import ActivityContainer from '../../../components/Dashboard/Activities/ActivityContainer';
+import DateButton from '../../../components/Dashboard/Activities/DateButton';
 import PageTitle from '../../../components/Dashboard/PageTitle';
 import UnauthorizedMessage from '../../../components/Dashboard/UnauthorizedMessage';
 import UnauthorizedMessageContainer from '../../../components/Dashboard/UnauthorizedMessageContainer';
+import useDates from '../../../hooks/api/useDate';
 import usePayment from '../../../hooks/api/usePayment';
 
 export default function Activities() {
   const { payment } = usePayment();
+  const { dates } = useDates();
+  const [dateName, setDateName] = useState('');
+
+  function handleDateSelection(name) {
+    setDateName(name);
+  }
 
   if (!payment) {
     return (
@@ -34,5 +44,20 @@ export default function Activities() {
     );
   }
 
-  return <h1>Página de atividades em construção!</h1>;
+  return (
+    <>
+      <PageTitle>Escolha de atividades</PageTitle>
+      <ActivityContainer>
+        {dates.map((date) => (
+          <DateButton 
+            key={date.id} 
+            onClick={() => handleDateSelection(date.name)} 
+            selected={date.name === dateName}
+          >
+            {date.name}
+          </DateButton>
+        ))}
+      </ActivityContainer>
+    </>
+  );
 }
---
+++ src/services/dateApi.js
@@ -0,0 +1,10 @@
+import api from './api';
+
+export async function getDates(token) {
+  const response = await api.get('/dates', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+}
